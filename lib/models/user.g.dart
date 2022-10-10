d6298450dc1a5c586684f1fcebcdd448bfd4d63a
cuwang <17621663320@163.com>
2022-10-10 16:59:38 +0800

    创建用户json

---
+++ lib/models/user.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User()
+  ..name = json['name'] as String
+  ..age = json['age'] as num
+  ..sex = json['sex'] as String
+  ..avatar = json['avatar'] as String;
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'name': instance.name,
+      'age': instance.age,
+      'sex': instance.sex,
+      'avatar': instance.avatar,
+    };
